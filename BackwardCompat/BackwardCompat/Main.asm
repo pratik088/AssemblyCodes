b971dd4759b46ab88a5150d5cea1b2b24f008954
Pratik Dineshkumar Solanki <pratik.solanki312@gmail.com>
2022-01-20 12:11:15 +0530

    .code main	proc 		mov rax,2 		mov rbx,3 		mov rcx,4 		ret main endp end

--- BackwardCompat/BackwardCompat/Main.asm
+++ BackwardCompat/BackwardCompat/Main.asm
@@ -1,8 +1,20 @@
+;.code
+;main	proc
+;		mov rax,2
+;		mov rbx,3
+;		mov rcx,4
+;		ret
+;main endp
+;end
+
+.386
+.model flat
 .code
+
 main	proc
-		mov rax,2
-		mov rbx,3
-		mov rcx,4
+		mov eax,2
+		mov ebx,3
+		mov ecx,4
 		ret
-main endp
-end
+main	endp
+end		main
